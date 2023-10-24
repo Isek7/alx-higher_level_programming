eeab4f50686cb678359dd8d38975124c1e203225
Isek7 <bengabrielisek@gmail.com>
2023-10-24 04:59:53 -0700

    Update

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,39 +1,35 @@
 #!/usr/bin/python3
-
-"""class definition"""
-
-
 class Square:
-    """A class definition of a square"""
-
+    """ A class that defines a square by its size
+    """
     def __init__(self, size=0):
-        """Initialize the square size"""
-
+        """ Method to initialize the square object
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
-        self.__size = size
+    def area(self):
+        """ Method that returns the square are of the object
+        """
+        return (self.__size ** 2)
 
     @property
     def size(self):
-        """Initialize the getter for size"""
-
+        """ Method to returns the size value
+        """
         return self.__size
 
     @size.setter
-    def size(self, size):
-        """Initialize the setter for size"""
-
-        if not isinstance(size, int):
+    def size(self, value):
+        """ Method to set the size value of the square object
+        """
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-
-        self.__size = size
-
-    def area(self):
-        """Compute the area of the square."""
-
-        return self.__size * self.__size 
+        else:
+            self.__size = value
